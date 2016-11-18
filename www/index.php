31483fcfa18c2fb2af13a91210adf50af0004fde
arith-alexander <alexander.k@arith-metic.jp>
2016-11-18 14:38:43 +0900

    #6 キャラをページに表示した

--- www/index.php
+++ www/index.php
@@ -6,9 +6,33 @@
         <link rel="stylesheet" type="text/css" href="style.css">
         <meta charset='utf-8'>
         <title>Docker</title>
+
+        <!-- Live2D Library -->
+        <script src="/live2d/lib/live2d.min.js"></script>
+
+        <!-- Live2D Framework -->
+        <script src="/live2d/framework/Live2DFramework.js"></script>
+
+        <!-- User's Script -->
+        <script src="/src/utils/MatrixStack.js"></script>
+        <script src="/src/utils/ModelSettingJson.js"></script>
+        <script src="/src/PlatformManager.js"></script>
+        <script src="/src/LAppDefine.js"></script>
+        <script src="/src/LAppModel.js"></script>
+        <script src="/src/LAppLive2DManager.js"></script>
+        <script src="/src/SampleApp1.js"></script>
     </head>
 
-    <body>
+    <body onload="sampleApp1()">
+
+
+        <div>
+            <canvas id="glcanvas" width="680" height="940"
+                style="border:dashed 1px #CCC">
+            </canvas>
+        </div>
+
+        <div id="myconsole" style="color:#000">---- Log ----</div>
 
     </body>
 </html>
