73c0a57ca3512ced8b26de46bed48056dc80aa13
arith-y-ono <y.ono@arith-metic.jp>
2016-11-18 17:09:20 +0900

    #5 bodyが二つあったので修正

--- www/index.php
+++ www/index.php
@@ -26,35 +26,34 @@
 
     </head>
 
-    <body>
-
-    <!-- modal menu -->
-    <div id="modal-content">
-          <a id="modal-close"　class="button-link">✖︎</a>
-          <table class="modal-table">
-    	        <tr>
-                <td><a href="menu1-1"><img src="" alt="1-1"></a></td>
-                <td><a href="menu1-2"><img src="" alt="1-2"></a></td>
-                <td><a href="menu1-3"><img src="" alt="1-3"></a></td>
-              </tr>
-              <tr>
-                <td><a href="menu2-1"><img src="" alt="2-1"></a></td>
-                <td><a href="menu2-2"><img src="" alt="2-2"></a></td>
-                <td><a href="menu2-3"><img src="" alt="2-3"></a></td>
-              </tr>
-              <tr>
-                <td><a href="menu3-1"><img src="" alt="3-1"></a></td>
-                <td><a href="menu3-2"><img src="" alt="3-2"></a></td>
-                <td><a href="menu3-3"><img src="" alt="3-3"></a></td>
-              </tr>
-        </table>
-    </div>
-    <p><a id="modal-open" class="button-link" style="right:2%; top:2%;position:absolute"><img src="" alt="MENU"></a></p>
-    <!-- modal menu -->
-
     <body onload="sampleApp1()">
         <div id="canvasContainer"><canvas id="glcanvas" style="width:100%" width="1000" height="600" style="border:dashed 1px #CCC"></canvas></div>
         <div id="myconsole" style="color:#000">---- Log ----</div>
+
+        <!-- modal menu -->
+        <div id="modal-content">
+              <a id="modal-close"　class="button-link">✖︎</a>
+              <table class="modal-table">
+        	        <tr>
+                    <td><a href="menu1-1"><img src="" alt="1-1"></a></td>
+                    <td><a href="menu1-2"><img src="" alt="1-2"></a></td>
+                    <td><a href="menu1-3"><img src="" alt="1-3"></a></td>
+                  </tr>
+                  <tr>
+                    <td><a href="menu2-1"><img src="" alt="2-1"></a></td>
+                    <td><a href="menu2-2"><img src="" alt="2-2"></a></td>
+                    <td><a href="menu2-3"><img src="" alt="2-3"></a></td>
+                  </tr>
+                  <tr>
+                    <td><a href="menu3-1"><img src="" alt="3-1"></a></td>
+                    <td><a href="menu3-2"><img src="" alt="3-2"></a></td>
+                    <td><a href="menu3-3"><img src="" alt="3-3"></a></td>
+                  </tr>
+            </table>
+        </div>
+        <p><a id="modal-open" class="button-link" style="right:2%; top:2%;position:absolute"><img src="" alt="MENU"></a></p>
+        <!-- modal menu -->
+
         <div class="footer">
             <a href="LESSON" >
                 <img src="" alt="LESSON" style="margin-left:20%">
